5ae266ce2facb7d015a7e0cd8ae3b3fefb480f66
armaango <armaan.goyal@gmail.com>
2017-03-08 17:28:29 -0800

    Settings functionality done

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,15 +1,36 @@
 import { Component } from '@angular/core';
-
+import { RedditService } from '../../app/services/reddit.service';
 import { NavController } from 'ionic-angular';
-
+import { RedditsPage } from '../reddits/reddits';
 @Component({
-  selector: 'settings',
-  templateUrl: 'settings.html'
+    selector: 'settings',
+    templateUrl: 'settings.html'
 })
 export class SettingsPage {
 
-  constructor(public navCtrl: NavController) {
+    category: any;
+    limit: any;
+    constructor(public navCtrl: NavController, private redditService: RedditService) {
+        this.getDefaults();
+    }
+    getDefaults() {
+        if (localStorage.getItem('category') != null) {
+            this.category = localStorage.getItem('category');
+        }
+        else {
+            this.category = 'sports';
+        }
+        if (localStorage.getItem('limit') != null) {
+            this.category = localStorage.getItem('limit');
+        }
+        else {
+            this.limit = 10;
+        }
 
-  }
-
+    }
+    setDefaults() {
+        localStorage.setItem('category', this.category);
+        localStorage.setItem('limit', this.limit);
+        this.navCtrl.push(RedditsPage);
+    }
 }
